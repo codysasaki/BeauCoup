9f55e85e4e21d8211622593e4dd426477b172248
codysasaki <40642438+codysasaki@users.noreply.github.com>
2021-02-25 12:37:51 -0800

    Update script.sh

--- py/script.sh
+++ py/script.sh
@@ -1,8 +1,8 @@
 for gamma in `seq 0.1 0.1 0.9`;
   do
-    python3 BeauCoup/py/compiler.py --gamma=$gamma queries.yaml IR_${gamma}.json
+    python3 ./py/compiler.py --gamma=$gamma query_examples.yaml IR_${gamma}.json
     for seed in `seq 1 16`;
       do
-	      python3 BeauCoup/py/simulator.py --seed=$seed IR_${gamma}.json /path/to/trace.npy outputs/report_gamma_${gamma}_seed_${seed}.npz
+	      python3 ./py/simulator.py --seed=$seed IR_${gamma}.json ./caida0.npy outputs/report_gamma_${gamma}_seed_${seed}.npz
       done
   done
